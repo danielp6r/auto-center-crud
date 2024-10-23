c9a603ad7ae0d4318297c6ac8e462e89c5428e93
Daniel Pereira <danielp6r@gmail.com>
2024-10-23 16:28:07 -0300

    Update Tela Produtos
    Formatando a entrada do campo txtValorUn. Ainda tem alguns bugs, porém já é possível utilizar.

--- src/main/java/Telas/ProdutoGUI.java
+++ src/main/java/Telas/ProdutoGUI.java
@@ -317,35 +317,36 @@ public class ProdutoGUI extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(lblDescricao)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(lblRs1)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(txtValorUn, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addComponent(lblValorUn, javax.swing.GroupLayout.Alignment.LEADING))
-                            .addGap(18, 18, 18)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(lblQuantidade, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(txtQuantidade, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGap(18, 18, 18)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(lblValorTotal)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(lblRs)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(lblSubtotal, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGap(39, 39, 39))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addComponent(btnInserirProduto)
-                            .addGap(18, 18, 18)
-                            .addComponent(lblCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(4, 4, 4)))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(txtDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addContainerGap())
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblDescricao)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lblRs1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(txtValorUn, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(lblValorUn, javax.swing.GroupLayout.Alignment.LEADING))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblQuantidade, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtQuantidade, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblValorTotal)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lblRs)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(lblSubtotal, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGap(39, 39, 39))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnInserirProduto)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(lblCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -372,13 +373,11 @@ public class ProdutoGUI extends javax.swing.JFrame {
                                         .addComponent(lblSubtotal, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
                                         .addComponent(lblRs)))
                                 .addComponent(lblValorTotal)))
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(lblQuantidade)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))))
+                        .addComponent(lblQuantidade)))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnInserirProduto)
-                    .addComponent(lblCancelar))
+                    .addComponent(lblCancelar)
+                    .addComponent(btnInserirProduto))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -499,21 +498,18 @@ public class ProdutoGUI extends javax.swing.JFrame {
                 txtValorUn.selectAll(); // Seleciona todo o texto
             }
         } else if (evt.getKeyChar() == ',') {
-            // Se a vírgula for digitada, selecionar as casas decimais
             String texto = txtValorUn.getText();
-            int indexVirgula = texto.indexOf(',');
 
-            if (indexVirgula != -1) {
-                // Se já existe uma vírgula, seleciona as casas decimais
-                txtValorUn.setSelectionStart(indexVirgula + 1); // Seleciona logo após a vírgula
-                txtValorUn.setSelectionEnd(texto.length()); // Seleciona até o final do texto
-            } else {
-                // Se não existe uma vírgula, adiciona a vírgula e seleciona as casas decimais
-                txtValorUn.setText(texto + ",");
-                txtValorUn.setSelectionStart(texto.length() + 1); // Move o cursor para a posição após a vírgula
-                txtValorUn.setSelectionEnd(texto.length() + 3); // Seleciona dois espaços para os centavos
-            }
-        }
+            // Remove a última vírgula ao digitar uma nova vírgula
+            int lastIndexVirgula = texto.lastIndexOf(',');
+            String novoTexto = texto.substring(0, lastIndexVirgula) + texto.substring(lastIndexVirgula + 1);
+
+            txtValorUn.setText(novoTexto);
+
+            // Seleciona as casas decimais
+            txtValorUn.setSelectionStart(lastIndexVirgula); // Seleciona logo após a vírgula
+            txtValorUn.setSelectionEnd(txtValorUn.getText().length()); // Seleciona até o final do texto
+        } 
     }//GEN-LAST:event_txtValorUnKeyReleased
 
     private void txtQuantidadeKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtQuantidadeKeyReleased
